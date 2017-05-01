341ed6294bebe309be0c0eb00475401c6dbeb90e
JackQueen <457883454@qq.com>
2017-05-01 08:47:46 +0800

    initial commit

--- app/src/main/java/com/wjq/smsreceiver/SMSBroadcastReceiver.java
+++ app/src/main/java/com/wjq/smsreceiver/SMSBroadcastReceiver.java
@@ -13,6 +13,6 @@ public class SMSBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        //收到广播后,启动服务上传信息内容
+        //收到广播后,启动服务上传信息内容1111
     }
 }
